b1896cd3297ea9584444cac877b178451f77ba6a
Mert Sincan <sincan.mert@gmail.com>
2024-06-20 16:40:06 +0100

    Update terser

--- packages/utils/rollup.config.mjs
+++ packages/utils/rollup.config.mjs
@@ -1,8 +1,8 @@
 import alias from '@rollup/plugin-alias';
 import { babel } from '@rollup/plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
 import postcss from 'rollup-plugin-postcss';
-import { terser } from 'rollup-plugin-terser';
 
 import fs from 'fs-extra';
 import path, { dirname } from 'path';
