2a740b88a62312d5816e198a16b814ef6802592f
Mert Sincan <sincan.mert@gmail.com>
2024-06-25 10:02:26 +0100

    Added alignOverlay method to utils

---
+++ packages/utils/src/dom/methods/alignOverlay.ts
@@ -0,0 +1,14 @@
+import absolutePosition from './absolutePosition';
+import getOuterWidth from './getOuterWidth';
+import relativePosition from './relativePosition';
+
+export function alignOverlay(overlay: HTMLElement, target: HTMLElement, appendTo: string, calculateMinWidth: boolean = true) {
+    if (overlay && target) {
+        if (appendTo === 'self') {
+            relativePosition(overlay, target);
+        } else {
+            calculateMinWidth && (overlay.style.minWidth = getOuterWidth(target) + 'px');
+            absolutePosition(overlay, target);
+        }
+    }
+}
