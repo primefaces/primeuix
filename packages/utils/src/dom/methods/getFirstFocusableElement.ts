9f62c83a3f4164e00e8fd5c62576d24be549baa5
Mert Sincan <sincan.mert@gmail.com>
2024-06-25 10:10:14 +0100

    Update getFirstFocusableElement.ts

--- packages/utils/src/dom/methods/getFirstFocusableElement.ts
+++ packages/utils/src/dom/methods/getFirstFocusableElement.ts
@@ -1,7 +1,7 @@
 import getFocusableElements from './getFocusableElements';
 
-export default function getFirstFocusableElement(element: Element, selector?: string): Element | undefined {
+export default function getFirstFocusableElement(element: Element, selector?: string): Element | null {
     const focusableElements = getFocusableElements(element, selector);
 
-    return focusableElements.length > 0 ? focusableElements[0] : undefined;
+    return focusableElements.length > 0 ? focusableElements[0] : null;
 }
