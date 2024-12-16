25ce578bc4de49fe95762b4cacbcf5a2ed448b12
Mert Sincan <sincan.mert@gmail.com>
2024-12-16 00:23:28 +0000

    Fixed #29 - Improve appendChild method in utils

--- packages/utils/src/dom/methods/appendChild.ts
+++ packages/utils/src/dom/methods/appendChild.ts
@@ -1,7 +1,7 @@
-import toElement from './toElement';
+import getTargetElement from './getTargetElement';
 
-export default function appendChild(element: unknown, child: Node) {
-    const target: Element | undefined = toElement(element);
+export default function appendChild(element: unknown, child: Node | Element) {
+    const target: Document | Element | null | undefined = getTargetElement(element, child as Element) as Exclude<ReturnType<typeof getTargetElement>, Window>;
 
     if (target) target.appendChild(child);
     else throw new Error('Cannot append ' + child + ' to ' + element);
