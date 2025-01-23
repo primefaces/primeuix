7c7a679f1c7e7c6fe64ffcc74a5aa357f7e7dd20
Mert Sincan <sincan.mert@gmail.com>
2025-01-23 14:32:13 +0000

    refactor: update modulePathIgnorePatterns to include submodules

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,7 @@ export default {
     rootDir: './',
     preset: 'ts-jest',
     testEnvironment: 'node',
-    modulePathIgnorePatterns: ['<rootDir>/packages/.*/dist'],
+    modulePathIgnorePatterns: ['<rootDir>/packages/.*/dist', '<rootDir>/submodules/.*'],
     moduleNameMapper: {
         '^@primeuix/(.*)/(.*)$': '<rootDir>/packages/$1/src/$2'
     }
