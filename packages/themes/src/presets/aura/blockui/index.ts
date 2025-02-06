ccc59b04e7eaa320cf9fc11f456195e30c291082
Mert Sincan <sincan.mert@gmail.com>
2025-02-06 02:45:39 +0000

    refactor: enhance blockui token structure with design tokens export

--- packages/themes/src/presets/aura/blockui/index.ts
+++ packages/themes/src/presets/aura/blockui/index.ts
@@ -1,5 +1,9 @@
-import type { BlockUITokenSections } from '@primeuix/themes/types/blockui';
+import type { BlockUIDesignTokens, BlockUITokenSections } from '@primeuix/themes/types/blockui';
 
 export const root: BlockUITokenSections.Root = {
     borderRadius: '{content.border.radius}'
 };
+
+export default {
+    root
+} satisfies BlockUIDesignTokens;
