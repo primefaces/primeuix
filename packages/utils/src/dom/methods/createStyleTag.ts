64f5a779b970a0287734bb544e8bd75b92031504
Mert Sincan <sincan.mert@gmail.com>
2025-02-11 12:59:34 +0000

    refactor: deprecate `createStyleTag`

--- packages/utils/src/dom/methods/createStyleTag.ts
+++ packages/utils/src/dom/methods/createStyleTag.ts
@@ -1,15 +1,8 @@
-import setAttributes from './setAttributes';
+import createStyleElement from './createStyleElement';
 
-export default function createStyleTag(attributes: { [key: string]: any } = {}, container: Element): HTMLStyleElement {
-    const element = document.createElement('style');
-
-    setAttributes(element, attributes);
-
-    if (!container) {
-        container = document.head;
-    }
-
-    container.appendChild(element);
-
-    return element;
+/**
+ * @deprecated Use `createStyleElement` instead.
+ */
+export default function createStyleTag(attributes: Record<string, unknown> = {}, container?: Element): HTMLStyleElement {
+    return createStyleElement('', attributes, container || document.head);
 }
