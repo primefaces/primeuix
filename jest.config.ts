7441b2389937a81810bdee29c81c2f30cf1c727a
Mert Sincan <sincan.mert@gmail.com>
2025-02-13 13:16:38 +0000

    chore: update jest configuration to include module file extensions and type safety

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,13 @@
+import type { Config } from 'jest';
+import { defaults } from 'jest-config';
+
 export default {
     rootDir: './',
     preset: 'ts-jest',
     testEnvironment: 'node',
     modulePathIgnorePatterns: ['<rootDir>/packages/.*/dist', '<rootDir>/submodules/.*'],
+    moduleFileExtensions: [...defaults.moduleFileExtensions, 'mts'],
     moduleNameMapper: {
         '^@primeuix/(.*)/(.*)$': '<rootDir>/packages/$1/src/$2'
     }
-};
+} satisfies Config;
