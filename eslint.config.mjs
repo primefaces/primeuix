655b3add39339c4ebccaf117e2a6cec2ba808b1b
Mert Sincan <sincan.mert@gmail.com>
2025-02-20 13:27:34 +0000

    fix: refactor eslint configuration for improved structure and clarity

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,33 +3,35 @@ import prettierConfig from 'eslint-config-prettier';
 import jest from 'eslint-plugin-jest';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config({
-    files: ['**/*.{js,mjs,ts,mts,d.ts}'],
-    extends: [eslint.configs.recommended, tseslint.configs.recommended, prettierConfig],
-    ignores: ['**/dist/**', 'submodules/**', '**/node_modules/**'],
-    rules: {
-        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-        'no-fallthrough': 'off',
-        'padding-line-between-statements': [
-            'error',
-            { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
-            { blankLine: 'any', prev: ['const', 'let', 'var'], next: ['const', 'let', 'var'] },
-            { blankLine: 'any', prev: ['case', 'default'], next: 'break' },
-            { blankLine: 'any', prev: 'case', next: 'case' },
-            { blankLine: 'always', prev: '*', next: 'return' },
-            { blankLine: 'always', prev: 'block', next: '*' },
-            { blankLine: 'always', prev: '*', next: 'block' },
-            { blankLine: 'always', prev: 'block-like', next: '*' },
-            { blankLine: 'always', prev: '*', next: 'block-like' },
-            { blankLine: 'always', prev: ['import'], next: ['const', 'let', 'var'] }
-        ]
-    },
-    overrides: [
-        {
-            files: ['**/__tests__/**/*', '**/*.test.*'],
-            extends: [jest.configs['flat/recommended']],
-            rules: {}
+export default tseslint.config([
+    {
+        files: ['**/*.{js,mjs,ts,mts,d.ts}'],
+        extends: [eslint.configs.recommended, tseslint.configs.recommended, prettierConfig],
+        rules: {
+            'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+            'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+            'no-fallthrough': 'off',
+            'padding-line-between-statements': [
+                'error',
+                { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
+                { blankLine: 'any', prev: ['const', 'let', 'var'], next: ['const', 'let', 'var'] },
+                { blankLine: 'any', prev: ['case', 'default'], next: 'break' },
+                { blankLine: 'any', prev: 'case', next: 'case' },
+                { blankLine: 'always', prev: '*', next: 'return' },
+                { blankLine: 'always', prev: 'block', next: '*' },
+                { blankLine: 'always', prev: '*', next: 'block' },
+                { blankLine: 'always', prev: 'block-like', next: '*' },
+                { blankLine: 'always', prev: '*', next: 'block-like' },
+                { blankLine: 'always', prev: ['import'], next: ['const', 'let', 'var'] }
+            ]
         }
-    ]
-});
+    },
+    {
+        files: ['**/__tests__/**/*', '**/*.test.*'],
+        extends: [jest.configs['flat/recommended']],
+        rules: {}
+    },
+    {
+        ignores: ['**/dist/**', '**/node_modules/**', '**/submodules/**']
+    }
+]);
