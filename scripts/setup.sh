2d7a0840a31b644311cea9d5fb36aae98e35a876
Mert Sincan <sincan.mert@gmail.com>
2025-03-28 22:39:40 +0000

    refactor: update setup script to link additional PrimeUI packages

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -10,8 +10,11 @@ case $1 in
     vue)
         cd submodules/primevue && pnpm run setup && \
         cd apps/showcase && pnpm link @primeuix/themes @primeuix/styles --global && \
-        cd ../../packages/themes && pnpm link @primeuix/themes @primeuix/styles --global && \
-        cd ../primevue && pnpm link @primeuix/themes @primeuix/styles --global
+        cd ../../packages/core && pnpm link @primeuix/styled @primeuix/utils --global && \
+        cd ../themes && pnpm link @primeuix/themes @primeuix/styles --global && \
+        cd ../core && pnpm link @primeuix/styled @primeuix/utils --global && \
+        cd ../primevue && pnpm link @primeuix/themes @primeuix/styles @primeuix/utils --global && \
+        cd ../forms && pnpm link @primeuix/forms @primeuix/utils --global
         ;;
     *)
         echo "Usage: $0 {ng|vue}"
