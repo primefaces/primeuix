6d53cf0947be9490e3ad6c1f1321eba94d47a0eb
Eason Lin <uu9924079@gmail.com>
2025-05-03 19:41:35 +0800

    fix(tabs): allow disabled tab to display tooltips

--- packages/styles/src/tabs/index.ts
+++ packages/styles/src/tabs/index.ts
@@ -102,6 +102,11 @@ export const style: StyleType = ({ dt }) => `
     outline-color: transparent;
 }
 
+.p-tab.p-disabled {
+    pointer-events: auto;
+    cursor: default;
+}
+
 .p-tab:not(.p-disabled):focus-visible {
     z-index: 1;
     box-shadow: ${dt('tabs.tab.focus.ring.shadow')};
