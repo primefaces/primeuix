f498772388d0eeae0c8767552da2522bbc5c152b
iLan Epstein <stuff@mentalfabrications.com>
2025-05-20 17:06:07 -0700

    getHiddenElementDimensions should clean up after itself

--- packages/utils/src/dom/methods/getHiddenElementDimensions.ts
+++ packages/utils/src/dom/methods/getHiddenElementDimensions.ts
@@ -2,12 +2,14 @@ export default function getHiddenElementDimensions(element?: HTMLElement): { wid
     const dimensions: { width: number; height: number } = { width: 0, height: 0 };
 
     if (element) {
+        const priorVisibility = element.style.visibility;
+        const priorDisplay = element.style.display;
         element.style.visibility = 'hidden';
         element.style.display = 'block';
         dimensions.width = element.offsetWidth;
         dimensions.height = element.offsetHeight;
-        element.style.display = 'none';
-        element.style.visibility = 'visible';
+        element.style.display = priorDisplay;
+        element.style.visibility = priorVisibility;
     }
 
     return dimensions;
