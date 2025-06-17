7210935cfb99c27ce1da11056373781b5b74466e
Mert Sincan <sincan.mert@gmail.com>
2025-06-17 11:08:25 +0100

    refactor: improve hidden element dimension methods to restore visibility and display styles

--- packages/utils/src/dom/methods/getHiddenElementOuterHeight.ts
+++ packages/utils/src/dom/methods/getHiddenElementOuterHeight.ts
@@ -1,11 +1,14 @@
 export default function getHiddenElementOuterHeight(element: HTMLElement): number {
     if (element) {
+        const [visibility, display] = [element.style.visibility, element.style.display];
+
+        // Temporarily hide the element to get its outer height
         element.style.visibility = 'hidden';
         element.style.display = 'block';
         const elementHeight = element.offsetHeight;
 
-        element.style.display = 'none';
-        element.style.visibility = 'visible';
+        element.style.display = display;
+        element.style.visibility = visibility;
 
         return elementHeight;
     }
