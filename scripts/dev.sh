046206545a8d873a8a3a6173dca458aeda51ff49
Mert Sincan <sincan.mert@gmail.com>
2025-06-19 09:55:27 +0100

    feat: enhance setup script to run concurrently and open local servers for ng, vue, and react

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -6,13 +6,13 @@ sleep 2
 
 case $1 in
     ng)
-        cd submodules/primeng && pnpm run dev
+        cd submodules/primeng && pnpm run dev & sleep 2 && open http://localhost:4200
         ;;
     vue)
-        cd submodules/primevue && pnpm run hot:dev
+        cd submodules/primevue && pnpm run hot:dev & sleep 2 && open http://localhost:3000
         ;;
     react)
-        cd submodules/primereact && pnpm run dev
+        cd submodules/primereact && pnpm run dev2 & sleep 2 && open http://localhost:3001
         ;;
     *)
         echo "Usage: $0 {ng|vue|react}"
