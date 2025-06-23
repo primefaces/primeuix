9a4be645046320a57568739d53dedd4b17959266
Cagatay Civici <cagatay.civici@gmail.com>
2025-06-23 15:43:56 +0300

    fix: fixed css precedense to make sure split button overrides button

--- packages/styles/src/splitbutton/index.ts
+++ packages/styles/src/splitbutton/index.ts
@@ -5,23 +5,23 @@ export const style = /*css*/ `
         border-radius: dt('splitbutton.border.radius');
     }
 
-    .p-splitbutton-button {
+    .p-splitbutton-button.p-button {
         border-start-end-radius: 0;
         border-end-end-radius: 0;
         border-inline-end: 0 none;
     }
 
-    .p-splitbutton-button:focus-visible,
-    .p-splitbutton-dropdown:focus-visible {
+    .p-splitbutton-button.p-button:focus-visible,
+    .p-splitbutton-dropdown.p-button:focus-visible {
         z-index: 1;
     }
 
-    .p-splitbutton-button:not(:disabled):hover,
-    .p-splitbutton-button:not(:disabled):active {
+    .p-splitbutton-button.p-button:not(:disabled):hover,
+    .p-splitbutton-button.p-button:not(:disabled):active {
         border-inline-end: 0 none;
     }
 
-    .p-splitbutton-dropdown {
+    .p-splitbutton-dropdown.p-button {
         border-start-start-radius: 0;
         border-end-start-radius: 0;
     }
@@ -34,12 +34,12 @@ export const style = /*css*/ `
         display: flex;
     }
 
-    .p-splitbutton-rounded .p-splitbutton-dropdown {
+    .p-splitbutton-rounded .p-splitbutton-dropdown.p-button {
         border-start-end-radius: dt('splitbutton.rounded.border.radius');
         border-end-end-radius: dt('splitbutton.rounded.border.radius');
     }
 
-    .p-splitbutton-rounded .p-splitbutton-button {
+    .p-splitbutton-rounded .p-splitbutton-button.p-button {
         border-start-start-radius: dt('splitbutton.rounded.border.radius');
         border-end-start-radius: dt('splitbutton.rounded.border.radius');
     }
