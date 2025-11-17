fac7e814fab301a72a9a600d341735f0c569d1ba
Phong6698 <Phong@hotmail.ch>
2025-11-17 10:17:21 +0100

    fix: missing styling for .p-dialog-content #183

--- packages/styles/src/dialog/index.ts
+++ packages/styles/src/dialog/index.ts
@@ -12,6 +12,7 @@ export const style = /*css*/ `
     .p-dialog-content {
         overflow-y: auto;
         padding: dt('dialog.content.padding');
+        flex-grow: 1;
     }
 
     .p-dialog-header {
